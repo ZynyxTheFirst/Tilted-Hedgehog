64ff94951297aa3eab12cf997a7eb98b4d18d3fa
Jervaro <jeremyvaudan@hotmail.com>
2023-04-14 20:23:25 +0200

    added back button on level select scene

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -7,16 +7,16 @@ using UnityEngine.SceneManagement;
 public class LevelSelector : MonoBehaviour
 {
     public int level;
-    public Text levelText;
+    //public Text levelText;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        levelText.text = level.ToString();
+        //levelText.text = level.ToString();
     }
 
     public void OpenScene(){
-        SceneManager.LoadScene("Level " + level.ToString());
+        SceneManager.LoadScene(level);
     }
 }

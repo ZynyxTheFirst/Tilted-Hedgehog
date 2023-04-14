f627baf6c3ed2e7b3a768f3670bed0f44c09bb25
Jervaro <jeremyvaudan@hotmail.com>
2023-04-14 12:16:31 +0200

    added 8 placeholder levels

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelSelector : MonoBehaviour
 {
+    public int level;
+    public Text levelText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        levelText.text = level.ToString();
     }
 
     public void OpenScene(){
-        SceneManager.LoadScene("Level 1");
+        SceneManager.LoadScene("Level " + level.ToString());
     }
 }

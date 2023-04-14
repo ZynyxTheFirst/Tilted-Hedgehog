e664936c9e83b4f5e9ad5ace2ffabac8ead2d014
Jervaro <jeremyvaudan@hotmail.com>
2023-04-14 12:03:52 +0200

    added level 1 and button to take u to lvl 1

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LevelController : MonoBehaviour
+public class LevelSelector : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -10,9 +11,7 @@ public class LevelController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OpenScene(){
+        SceneManager.LoadScene("Level 1");
     }
 }

05a0d913aa082b87872355648130f808f1008463
Robin W <rejjkill.redunzl@gmail.com>
2023-04-16 21:51:25 +0200

    change project settings for android

--- Assets/Scripts/Controlls/TouchInput.cs
+++ Assets/Scripts/Controlls/TouchInput.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class TouchInput : MonoBehaviour
 {
-    private float desiredRot;
-    public float rotSpeed = 250;
+    private float desiredRotation;
+    public float rotationSpeed = 250;
     public float damping = 10;
 
     private void OnEnable()
     {
-        desiredRot = transform.eulerAngles.z;
+        desiredRotation = transform.eulerAngles.z;
     }
 
     void Update()
     {
+        ///Actual koden för mobiler
         /*if(Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -24,13 +25,14 @@ public class TouchInput : MonoBehaviour
         var desiredRotQ = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, desiredRot);
         transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotQ, Time.deltaTime * damping);
         */
+        ///Kod för att testa på datorn **Ska tas bort**
         if (Input.GetMouseButton(0))
         {
-            if (Input.mousePosition.x > Screen.width / 2) desiredRot -= rotSpeed * Time.deltaTime;
-            else desiredRot += rotSpeed * Time.deltaTime;
+            if (Input.mousePosition.x > Screen.width / 2) desiredRotation -= rotationSpeed * Time.deltaTime;
+            else desiredRotation += rotationSpeed * Time.deltaTime;
         }
 
-        var desiredRotQ = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, desiredRot);
-        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotQ, Time.deltaTime * damping);
+        var desiredRotationQuaternion = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, desiredRotation);
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotationQuaternion, Time.deltaTime * damping);
     }
 }

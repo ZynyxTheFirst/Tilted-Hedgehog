3918ab58428d1ac8e42aa6ee7a51cab3eb122c76
Jervaro <jeremyvaudan@hotmail.com>
2023-04-16 18:41:24 +0200

    begun adding end goal and score screen popup

---
+++ Assets/Scripts/ScoreWindow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreWindow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = Vector2.zero;
+    }
+
+    public void Open(){
+        transform.LeanScale(Vector2.one, 0.8f);
+    }
+
+    public void Close(){
+        transform.LeanScale(Vector2.zero, 1f).setEaseInBack();
+    }
+}

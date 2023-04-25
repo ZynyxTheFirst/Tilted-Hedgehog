2c0175d746e01795249957b9fcc63f58008bffc3
FilipWil <fille_kbk@hotmail.com>
2023-04-25 16:16:53 +0200

    Deathzones
    Either sends the player to an assigned checkpoint or restarts the level
    
    Attach to the zone that kills the player

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Transform respawnPoint;
+
+
+    //Sends you to checkpoint 
+
+    //private void OnTriggerEnter2D(Collider2D other) 
+    //{
+    //    if(other.gameObject.CompareTag("Player"))
+    //    {
+    //        player.transform.position = respawnPoint.position;
+    //    }
+    //}
+
+    //Restarts the scene
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+}

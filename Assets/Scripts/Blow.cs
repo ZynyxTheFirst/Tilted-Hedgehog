e19546039e75b23df3cba2a872b8da9a25ee389f
theepicone1997 <112469278+theepicone1997@users.noreply.github.com>
2023-04-26 16:07:23 +0200

    Some aditional code adjustments

--- Assets/Scripts/Blow.cs
+++ Assets/Scripts/Blow.cs
@@ -6,15 +6,21 @@ public class Blow : MonoBehaviour
 {
     public ParticleSystem particles;
 
+    public GameObject player;
+
     public float windSpeed = 2.0f;
+
+        void Start()
+    {
+        particles.Stop();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            player.GetComponent<Rigidbody2D>().gravityScale = -1.0f;
             particles.Play();
-            Vector3 directionOfWind = new Vector3(0f, 1.0f, 0f);
-            particles.GetComponent<Rigidbody2D>().AddForce(directionOfWind * windSpeed);
         }
             
     }
@@ -23,7 +29,13 @@ public class Blow : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+           /*  InvokeRepeating("RestoreGravityScale", 0.1f, 0.1f); */
+            player.GetComponent<Rigidbody2D>().gravityScale = 3.3f;
             particles.Stop();
         }
+    }
+
+    private void RestoreGravityScale(){
+        player.GetComponent<Rigidbody2D>().gravityScale =+ 0.1f;
     }
 }
--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -19,13 +19,5 @@ public class CharacterManager : MonoBehaviour
         if (gameObject.GetComponent<Rigidbody2D>().velocity.magnitude > 0.125) animator.SetBool("isMoving", true);
         else animator.SetBool("isMoving", false);
     }
-
-    void OnCollisionEnter(Collision other) {
-    if (other.gameObject.CompareTag("Wind")) {
-        print("Hej");
-        Vector3 windDirection = other.gameObject.transform.up;
-        float windSpeed = 10.0f;
-        GetComponent<Rigidbody>().AddForce(windDirection * windSpeed, ForceMode.Impulse);
-    }
-}
+    
 }

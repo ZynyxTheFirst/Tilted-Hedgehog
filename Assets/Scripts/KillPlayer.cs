a8fa5d9987b29ac11a1306a90ca84362a0e1744e
Jervaro <jeremyvaudan@hotmail.com>
2023-05-15 13:35:57 +0200

    fixade death animation

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -7,6 +7,9 @@ public class KillPlayer : MonoBehaviour
 {
     public GameObject player;
     public Transform respawnPoint;
+    public float time = 2f;
+    private Transform deathzoneTransform;
+    private Animator playerAnimator;
 
 
     //Sends you to checkpoint 
@@ -18,15 +21,29 @@ public class KillPlayer : MonoBehaviour
     //        player.transform.position = respawnPoint.position;
     //    }
     //}
-
+    private void Start()
+    {
+        deathzoneTransform = GetComponent<Transform>();
+        playerAnimator = player.GetComponent<Animator>();
+    }
     //Restarts the scene
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            GameManager.GameOver();
+            Invoke("CallGameOver", time);
             //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            player.GetComponent<Transform>().position = deathzoneTransform.position;
+            player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            player.GetComponent<MovePlayer>().enabled = false;
+            playerAnimator.SetBool("isDead", true);
+            Debug.Log(playerAnimator.GetBool("isDead"));
         }
+    }
+
+    private void CallGameOver()
+    {
+        GameManager.GameOver();
     }
 
 }

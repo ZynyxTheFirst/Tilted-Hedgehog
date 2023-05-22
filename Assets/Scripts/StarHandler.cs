b2e82b0f7fd2ba1616c30b003b778b04fa08e8f3
Jervaro <Jeremyvaudan@hotmail.com>
2023-05-22 09:51:10 +0200

    uppdatera star handler skript och endgoal skript

--- Assets/Scripts/StarHandler.cs
+++ Assets/Scripts/StarHandler.cs
@@ -13,7 +13,6 @@ public class StarHandler : MonoBehaviour
     public bool resetAllPlayerPref;
 
     private string currentLevelStars;
-    private bool starsActive;
     
 
     void Start()
@@ -23,10 +22,6 @@ public class StarHandler : MonoBehaviour
         }
         currentLevelStars = "StarsCollected" + SceneManager.GetActiveScene().buildIndex;
 
-        // Temporär, ska egentligen sättas första gången leveln laddas och endast en gång.
-        //PlayerPrefs.SetInt(currentLevelStars, 0);
-        
-
         for (int i = 0; i <= PlayerPrefs.GetInt(currentLevelStars); i++)
         {
             if(starsPickup[i].GetComponent<PickupStar>().IsCollected()){
@@ -34,21 +29,5 @@ public class StarHandler : MonoBehaviour
             }
             
         }
-        
-        
-    }
-
-    private void Update()
-    {
-        //amountCollected = PlayerPrefs.GetInt("StarsCollected");
-        /*
-        
-        for (int i = 0; i < starsPickup.Length; i++)
-        {
-            if(starsPickup[i].GetComponent<PickupStar>().IsCollected()){
-                starsUI[i].GetComponent<Image>().sprite = starCollected;
-            }
-        }
-        */
     }
 }

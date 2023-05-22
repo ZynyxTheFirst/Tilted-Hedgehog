ac114057184d817f52e4695eca28a8073cb08bd1
Jervaro <jeremyvaudan@hotmail.com>
2023-05-22 14:31:29 +0200

    uppdatera EndGoal
    Nu stannar spelaren när den når end goal

--- Assets/Scripts/EndGoal.cs
+++ Assets/Scripts/EndGoal.cs
@@ -5,15 +5,18 @@ public class EndGoal : MonoBehaviour
 {
     [SerializeField] GameObject scoreScreen;
     public GameObject starUI;
-    // [SerializeField] int levelToUnlock = 0;
-    private GameObject joystickArea;
     public GameObject[] starsScoreScreen;
+
+    private GameObject joystickArea;
+    private GameObject player;
     private GameObject[] starsPickup;
     private Sprite starCollected;
     private int amountCollected;
+    
 
     void Start()
     {
+        player = GameObject.Find("Character");
         joystickArea = GameObject.Find("JoystickArea");
         starCollected = scoreScreen.GetComponent<StarHandler>().starCollected;
         starsPickup = scoreScreen.GetComponent<StarHandler>().starsPickup;
@@ -26,8 +29,11 @@ public class EndGoal : MonoBehaviour
         if(joystickArea != null) { 
             joystickArea.SetActive(false);
         }
-        
 
+        // Fryser spelarkontrollerna och stoppar spelarens velocity.
+        player.GetComponent<MovePlayer>().enabled = false;
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        
         for (int i = 0; i < starsPickup.Length; i++){
             if(starsPickup[i].GetComponent<PickupStar>().IsCollected()){
                 amountCollected++;
@@ -53,6 +59,7 @@ public class EndGoal : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+
             OpenScoreScreen();
             starUI.SetActive(true);
             GetComponent<SpriteRenderer>().enabled = false;

55da777492b861c6498d112da9b6dfd83bb6c3f1
Jervaro <Jeremyvaudan@hotmail.com>
2023-05-24 09:39:58 +0200

    justerade anim på stars o end goal lite

--- Assets/Scripts/JeremyScripts/EndGoal.cs
+++ Assets/Scripts/JeremyScripts/EndGoal.cs
@@ -12,15 +12,15 @@ public class EndGoal : MonoBehaviour
     private GameObject[] starsPickup;
     private Sprite starCollected;
     private int amountCollected;
-    //private bool animatorOff = true;
+    private bool animatorOff = true;
     
     private void Update() {
-        /*
+        
         if(SceneTransition.centerReached && animatorOff){
             GetComponent<Animator>().enabled = true;
             animatorOff = false;
         }
-        */
+        
     }
 
     void Start()

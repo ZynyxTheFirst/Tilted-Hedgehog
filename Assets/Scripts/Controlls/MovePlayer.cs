c780f2881d8903d3b8b9241923a024bdc1b71059
Jervaro <Jeremyvaudan@hotmail.com>
2023-05-25 15:08:50 +0200

    la till clear playerprefs knapp i main menu

--- Assets/Scripts/Controlls/MovePlayer.cs
+++ Assets/Scripts/Controlls/MovePlayer.cs
@@ -6,17 +6,12 @@ public class MovePlayer : MonoBehaviour
     public MovementJoystick movementJoystick;
     public float playerSpeed;
     public float slipperyFactor; // controls the slippery movement
-    public bool resetAllPlayerPref;
     private Rigidbody2D rb;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        if (resetAllPlayerPref)
-        {
-            PlayerPrefs.DeleteAll();
-        }
 
         rb = GetComponent<Rigidbody2D>();
         if (rb == null)

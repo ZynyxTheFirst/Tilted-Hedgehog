5c8c3207ab45b81122285d409224ff2d096cfbd9
theepicone1997 <oscar-sture@live.se>
2023-05-26 15:39:49 +0200

    Added audio effects and music

--- Assets/Scripts/Controlls/MovePlayer.cs
+++ Assets/Scripts/Controlls/MovePlayer.cs
@@ -8,11 +8,19 @@ public class MovePlayer : MonoBehaviour
     public float slipperyFactor; // controls the slippery movement
     public bool resetAllPlayerPref;
     private Rigidbody2D rb;
+    [SerializeField] private AudioClip audioClip;
+
+    [SerializeField] private AudioSource audioSource;
+
+    private bool isAudioClipBeingPlayed;
+
+    private float speedThresholdForAudio = 0.3f;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        audioSource.clip = audioClip;
         if (resetAllPlayerPref)
         {
             PlayerPrefs.DeleteAll();
@@ -40,9 +48,36 @@ public class MovePlayer : MonoBehaviour
                 rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, Time.deltaTime * slipperyFactor);
             }
         }
-        
+        CheckCharacterMovment();
     }
 
+    void CheckCharacterMovment()
+    {
+        if (isCharacterMoving() && !isAudioClipBeingPlayed)
+        {
+            PlayAudio();
+        }
+        else if (!isCharacterMoving() && isAudioClipBeingPlayed)
+            StopAudio();
+    }
 
+    void PlayAudio()
+    {
+        audioSource.Play();
+        isAudioClipBeingPlayed = true;
+    }
 
+    void StopAudio()
+    {
+        audioSource.Stop();
+        isAudioClipBeingPlayed = false;
+    }
+
+    bool isCharacterMoving(){
+        if(rb.velocity.magnitude > speedThresholdForAudio){
+           return true;
+        }
+        else
+        return false;
+    }
 }
